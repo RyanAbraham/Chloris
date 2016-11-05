f174419243fe76c77dfb0fed3729c4092fe20239
VeronicaWitzig <vwitzig@uwo.ca>
2016-11-05 19:27:19 -0400

    adding bg

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_BGScroller : MonoBehaviour
+{
+	private float scrollSpeed = 5;
+	private float tileSizeZ = 1;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
