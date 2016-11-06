45a8d7d256ffee5c0f0f3bdcba9a64a64cd61112
Ryan Abraham <ryan_ja@live.ca>
2016-11-06 02:05:01 -0500

    Fixed coin and added cat

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,11 +8,14 @@ public class GameManager : MonoBehaviour
     public float fHigh = 10.0f;
     public float fLow = 0.2f;
     public GameObject coin;
+    public GameObject cat;
+    public Text coinText;
 
     private float curAccel = 0f;
     private float avgAccel = 0f;
     private int steps = 0;
     private int oldSteps;
+    private int coins = 0;
     private bool stateH = false;
     private GameObject[] pickups = new GameObject[3];
 
@@ -24,26 +27,17 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         avgAccel = Input.acceleration.magnitude;
+        coinText.text = "Coins: 0";
     }
 
     void Update()
     {
+
         transform.Rotate(m_RotationDirection * Time.deltaTime * m_Speed);
         if (steps % 10 == 1 && steps != oldSteps) {
             ToggleRotationDirection();
         }
         oldSteps = steps;
-    }
-
-    public void ToggleRotationDirection()
-    {
-        //Debug.Log("Toggling rotation direction");
-
-        if (m_RotationDirection == Vector3.up) {
-            m_RotationDirection = Vector3.down;
-        } else {
-            m_RotationDirection = Vector3.up;
-        }
     }
 
     void FixedUpdate()
@@ -56,8 +50,11 @@ public class GameManager : MonoBehaviour
             if (delta > hiLim) {
                 stateH = true;
                 steps++;
-                // Handle pickups[0]
                 if (pickups[0] != null) {
+                    if(pickups[0].CompareTag("Coin")) {
+                        coins++;
+                        coinText.text = "Coins: " + coins.ToString();
+                    }
                     Destroy(pickups[0]);
                 }
                 for (int i = 1; i < pickups.Length; i++) {
@@ -67,8 +64,12 @@ public class GameManager : MonoBehaviour
                         pickups[i] = null;
                     }
                 }
-                if (Random.Range(1,10) <= 2) {
-                    pickups[pickups.Length - 1] = Instantiate(coin);
+                if (Random.Range(1,7) >= 6) {
+                    if (Random.Range(1, 15) <= 13) {
+                        pickups[pickups.Length - 1] = Instantiate(coin);
+                    } else {
+                        pickups[pickups.Length - 1] = Instantiate(cat);
+                    }
                     pickups[2].transform.position = new Vector3(0, 2, -2);
                 }
             }
@@ -79,5 +80,16 @@ public class GameManager : MonoBehaviour
             }
         }
         //stepText.text = "CA: " + curAccel.ToString() + "\nAA: " + avgAccel.ToString() + "\nDl: " + delta.ToString() + "\nSteps: " + steps.ToString();
+    }
+
+    public void ToggleRotationDirection()
+    {
+        //Debug.Log("Toggling rotation direction");
+
+        if (m_RotationDirection == Vector3.up) {
+            m_RotationDirection = Vector3.down;
+        } else {
+            m_RotationDirection = Vector3.up;
+        }
     }
 }
